a42774e49b531d6107e964e1117bef4ebfcbd68e
Mohammad Mahhouk <m.mahhouk@tu-braunschweig.de>
2017-12-12 23:45:40 +0100

    tests

--- Scene/src/MainWindow.cpp
+++ Scene/src/MainWindow.cpp
@@ -1,3 +1,4 @@
+#include <sqlite3.h>
 #include "MainWindow.h"
 
 
@@ -220,9 +221,52 @@ void MainWindow::writeSettings()
     settings.setValue("size", size());
 }
 
+static int callback(void *data, int argc, char **argv, char **azColName){
+    int i;
+    fprintf(stderr, "%s: ", (const char*)data);
+
+    for(i = 0; i<argc; i++){
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+
+    printf("\n");
+    return 0;
+}
+
 void MainWindow::loadFile(const QString& fileName)
 {
-    if(fileName == tr("blabla") ) return;
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    char *sql;
+    const char* data = "Callback function called";
+
+    /* Open database */
+
+    rc = sqlite3_open(fileName.toStdString().c_str(), &db);
+
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return;
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    /* Create SQL statement */
+    sql = "SELECT * from Threads";
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+
+    if( rc != SQLITE_OK ) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+    sqlite3_close(db);
+
+
 }
 
 void MainWindow::closeEvent(QCloseEvent* event)

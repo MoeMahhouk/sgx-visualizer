779f01517508f78238d7acbe0068915aa6f0c6a9
Moe <mohammed-mahhouk@hotmail.com>
2018-01-17 12:31:07 +0100

    class name changing

---
+++ Scene/include/Filtering/SGX/ECallOCallFilter.h
@@ -0,0 +1,30 @@
+//
+// Created by moe on 10.01.18.
+//
+
+#ifndef SCENE_ECALLFILTER_H
+#define SCENE_ECALLFILTER_H
+
+
+#include <Filtering/IFilter.h>
+
+namespace moe {
+
+    class ECallOCallFilter : public IFilter {
+
+    public:
+        ECallOCallFilter(IReciever *reciever, QVector<int> chosenECalls, QVector<int> chosenOCalls);
+
+        QString toSQLStatement() override;
+
+        void execute() override;
+
+    private:
+        QVector<int> chosenECalls_;
+        QVector<int> chosenOCalls_;
+    };
+
+}
+
+
+#endif //SCENE_ECALLFILTER_H
---
+++ Scene/src/Filtering/SGX/ECallOCallFilter.cpp
@@ -0,0 +1,48 @@
+//
+// Created by moe on 10.01.18.
+//
+
+#include "Filtering/SGX/ECallOCallFilter.h"
+
+moe::ECallOCallFilter::ECallOCallFilter(IReciever *reciever, QVector<int> chosenECalls, QVector<int> chosenOCalls) :
+        IFilter(reciever), chosenECalls_(chosenECalls), chosenOCalls_(chosenOCalls)
+{
+
+}
+
+QString moe::ECallOCallFilter::toSQLStatement() {
+    QString conditionQuery = "";
+    conditionQuery.append(" AND ( (e1.type = 14 AND e1.call_id IN ( ");
+    if (!chosenECalls_.isEmpty())
+    {
+        for (int i = 0; i < chosenECalls_.size() ; ++i)
+        {
+            conditionQuery.append(QString::number(chosenECalls_[i]));
+            conditionQuery.append(" ,");
+        }
+        conditionQuery.remove(conditionQuery.size()-1, 1);
+        conditionQuery.append("))");
+    } else {
+        conditionQuery.append("))");
+    }
+    conditionQuery.append(" OR ( e1.type = 16 AND e1.call_id IN ( ");
+    if (!chosenOCalls_.isEmpty()) {
+        for (int i = 0; i < chosenOCalls_.size() ; ++i)
+        {
+            conditionQuery.append(QString::number(chosenOCalls_[i]));
+            conditionQuery.append(" ,");
+        }
+        conditionQuery.remove(conditionQuery.size()-1, 1);
+        conditionQuery.append("))");
+    } else {
+        conditionQuery.append("))");
+    }
+    conditionQuery.append(")");
+    return conditionQuery;
+}
+
+void moe::ECallOCallFilter::execute() {
+    dReciever_->SetAction(TYPES::ACTION_LIST::ECALLFILTER);
+    dReciever_->getResult(toSQLStatement());
+}
+
